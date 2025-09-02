110225f8b872db6fa5ef649500aafb09da6123db
isabellacr444 <00001114399279sp@al.educacao.sp.gov.br>
2025-09-02 11:03:19 -0300

    02.09

---
+++ src/products/dto/create-product.dto.ts
@@ -0,0 +1,12 @@
+
+import { IsString, IsNumber, IsNotEmpty, Min } from "class-validator";
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+    
+    @IsNumber()
+    @Min(0)// Preço não pode ser negativo
+  price: number;
+}
